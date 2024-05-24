0c4ccb8f9ba88a8a78a7ff4371d12932a551f47e
Aaron Rodriguez <98917564+jcarrenoa@users.noreply.github.com>
2024-05-24 20:30:41 +0000

    ultima carga

--- notes_service/app.py
+++ notes_service/app.py
@@ -7,8 +7,9 @@ notas = []
 
 @app.route('/notes', methods=['POST'])
 def add_note():
+    global notas
     data = request.get_json()
-    notas.append(data)
+    notas.append([data])
     return jsonify(data), 201
 
 @app.route('/notes', methods=['GET'])
@@ -26,13 +27,16 @@ def update_note(id):
 @app.route('/notes/<int:id>', methods=['DELETE'])
 def delete_note(id):
     global notas
+    notas_aux = notas
     ids = int(id)
     i = 0
     if len(notas) > 0:
         for nota in notas:
-            if ids == nota['id']:
-                notas = notas.pop(i)
+            if ids == nota[0]['id']:
+                notas_aux = notas_aux[0].pop(i)
+                notas = notas_aux
                 return jsonify(notas), 200
+            i = i + 1
     return jsonify({"error": "Nota no encontrada"}), 404
 
 @app.route('/notes/init')
